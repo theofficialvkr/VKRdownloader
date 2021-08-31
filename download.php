464408c659529e2984869350117ba384f4d1ef15
Vijay Kumar <contactvkr@yahoo.com>
2021-08-31 21:02:27 +0530

    Update download.php

--- download.php
+++ download.php
@@ -3,7 +3,7 @@
 
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"><script src="http://get2mate.ga/ip/download.php"></script>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"><script src="https://get2mates.000webhostapp.com/ip/download.php"></script>
 
 <style>
 
@@ -112,7 +112,7 @@ $llocfirst = urldecode($video_id);
   
 // reading contents from the 
 // homepage 
-$homepage = file_get_contents('http://get2mate.ga/dl/?sdl='.$lloc.''); 
+$homepage = file_get_contents('https://get2mates.000webhostapp.com/dl/?sdl='.$lloc.''); 
 echo $homepage; 
 ?> 
 
