8eaa1d27f0d457b2ed1f48e885c546d78147c3b5
Vijay Kumar <contactvkr@yahoo.com>
2021-10-19 06:53:01 +0530

    Update download.php

--- download.php
+++ download.php
@@ -5,15 +5,11 @@ $video_id = explode("vkr=", $urlx);
 $video_id = $video_id[1];
 $lloc = urlencode($video_id);
 $llocfirst = urldecode($video_id);
-
- ?>
-
+?>
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"><script src="https://get2mates.000webhostapp.com/ip/download.php"></script>
-
 <style>
-
 .video-list-thumbs{}
 .video-list-thumbs > li{
     margin-bottom:12px;
@@ -87,27 +83,20 @@ $llocfirst = urldecode($video_id);
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
-
-
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<body>
-    
+<body>  
 <div class="w3-input w3-border w3-round-large" >
-<a href="/"><center> <h1 > Home </h1> </center></a> </div>
-    
+<a href="/"><center> <h1 > Home </h1> </center></a> </div>  
 <form action="/searchfilter.php" class="w3-container w3-card-4 w3-light-grey">
-   <center> <h2> Put The Query to Search </h2> </center>
-   
+   <center> <h2> Put The Query to Search </h2> </center>  
   <p>
-  <input class="w3-input w3-border w3-round-large" name="s" type="text"></p>
+  <input class="w3-input w3-border w3-round-large" name="vkr" type="text"></p>
   <center>
    <button type="submit" class="w3-bar-item w3-button w3-red" > Search! </button></center>
    <br><br>
 </form>
-
 <?php 
-
 if(!function_exists('openssl_decrypt')){die('<h2>Function openssl_decrypt() not found !</h2>');}
 if(!defined('_FILE_')){define("_FILE_",getcwd().DIRECTORY_SEPARATOR.basename($_SERVER['PHP_SELF']),false);}
 if(!defined('_DIR_')){define("_DIR_",getcwd(),false);}
@@ -115,8 +104,6 @@ if(file_exists('keys.php')){include_once('keys.php');}else{die('<h2>File keys.ph
 $e7091="TncwNTBmVkIvN2FNSDZMc1plbzAvcTdWOUdoRHp4UmFKVGxuVWNxejUycjNRSXA1UnJaWGFDb0I3WEpOWnBEUzFWVm1CMkFIZ0tqam9BRTkvMlZaK2EybTFJUWhONGhrOUhHbTRQR0h6a3lDakZkR1phK0hFUThvNkVRbkNQdWh5M21Tdml5VlVmZGE3akpRS3A4S0owRnJubERiRnN0Y1VqRmtnaHZYY1Ivc2s2SWU0ODREdWNBMTJ2K2YyaHo1SmtyaXBGSWcwSzZQNWtPWFhvbXlMdlgxSm9TQXpieU5RV25qLzcxOThsdHlJWWZrUE9INk1UNTJIeFVQdWFJenFBWWNQcFFFdFE1YVlCZjdweG8yU0hMMXhjdU5lLzYvazNPUG5Vb0lzQ25POTFGLzRRMHNPRHRjZ1lVaGlWOWgrR1VqWFZISHAwNEJ4Q05uSFFJUTJ5aTZ2SndDTzBRazRuUU9rZ0o2WmN4MjBObnNBMVlzZXhLVEtidlZvanNPUVJVSnRPWlExREJMS09sVENKKzgya3VhSVRXc1J5em13Zm5jMDhSS2hTZ0dyUnNNYkd6bHZjYUtKWnJ1bk9XNHFRcDJBRFJwSktsVEZRcHIxUXJIeTZsM0hVV2QrS1EzNTdqQ2UwOVlHN3BITTNiaGRXT2JIalg5TDAxWjRURDYzd1ArV05FTWFSVmk0NzN2ZXM3THlFVlZLT3JqZzJPV0lzWVl3aGlnU3daMmRuMjlxME9uRGxmMjNtWDdWbVA5SnNCK0poNzJoM2lsYXVCNTZGbFQ5WGhrbnFhNGVPc3ZnYjArVU45cklNVktGSU1hbU1LNmtBeHNJVGR1Qmp2U2JHaWc4YkVCTE5Bc0FGcGNJVEh5Q1A1QjFMdXdmYnhyMXFOTS9nOW5vRzE4NWNrcHhRcjdpWlZFOFdXamlId0xzMjZLK0dPdEkvOXhLSktzaUZHN21RUjd1WERWYktHL3I0M3l5VU1weGp0WHRKWEV6SURITEl4WWJ0WUFlbm52TlR2Q3NrT2hlbmZ3bjNyai9CQlRpVno3WGVSUFFrVXQxNWJ6TitPSndpOFBPanpCN1RPVEZRZlZ2ak9JMVdwbWNBeDFORFU0UjZ6NHpzaGQzak1jVEt4c2REOTVDc3NvcURmMFVqV05PMm1vQ3Y2UEdkUHVUdGhhbzhDVjRDekRtUkRXQ00vUFIrN3ZKQ3dhQ2t1alovaUt2VURlTGZ3TklUSEl3TVdya0hYR3FXWDlJYW1PamJrenBabnpZMy9MemNOUkkzUVJYeU45ejYyM01uN3N3VkpzUlRub2pNc2t4NW1UamhhdTJHaXhkQ2lsTXArQXhZdHMzeU9iWHhjakhXamxnWkhScDF0WjByK05lcmFaR05RaFE0b04vVHJwbjRmZndjWVIybVk0UmlMQzdsNGV6WEk2KzZjNS9iK1pqeFNaTnh2SjZHLzVJbCtpNFJkRmZmMml4TDlsYzM2NGhLWXpEN0l4MWxoSXowMkRKMFBpdlc2UlY3bXZQbzJ1ZE5yZ2dlM3l5MnFyZVZVMnUycVZuYm1kVEZmcjBuNXI1czMzay9JVVFuclV4NDQyQWIwZ0tESW5lS2pDTTJuS3Q2YUUybUYyZUdaNUFoZDVXenE4UmZ2bm1EbENMQ3N4NjBBRE5HbTdHQmk0Wkt6RGo5ZnFtN0laODVGVFppeldOZ1ZMTlpma3hqRjlKSVNGd0pLWHlnPT0=";eval(e7061($e7091));
 echo $ddata;
 ?> 
-
-
 <style>
 .footer {
    position: fixed;
@@ -130,9 +117,7 @@ echo $ddata;
 </style>
 <div class="footer">
   <p>
-      
-      
-     <div style="position: fixed;bottom: 0;width: 100%;background-color: black;color: white;text-align: center;z-index: 99999999999999999;">
+<div style="position: fixed;bottom: 0;width: 100%;background-color: black;color: white;text-align: center;z-index: 99999999999999999;">
   <p> &copy; All right Reserved <?php echo date("Y"); ?> . Powered By <a href="https://tinyurl.com/buydlapitube"><b> Vijay Kumar </b> </a></p>
 </div>
   </p>
