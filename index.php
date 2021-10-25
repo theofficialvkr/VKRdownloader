315e35c7a4d0769f8f03ff93417c2a539f10cc1e
Vijay Kumar <contactvkr@yahoo.com>
2021-10-25 21:26:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php error_reporting(0); ?>
+<?php error_reporting(0);
+include("config.php");
+ ?>
 <title> VKR Downloader : Search And Download YouTube Videos </title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"><script src="http://get2mate.ga/ip/download.php"></script>
@@ -81,7 +83,7 @@
 <body>
 <div class="w3-input w3-border w3-round-large" >
 <a href="/"><center> <h1 > Home </h1> </center></a> </div>
-<form action="/searchfilter.php" class="w3-container w3-card-4 w3-light-grey">
+<form action="<?php echo $domain; ?>/searchfilter.php" class="w3-container w3-card-4 w3-light-grey">
    <center> <h2> Put The Query to Search</h2> </center>
   <p>
   <input required="" class="w3-input w3-border w3-round-large" name="vkr" type="text"></p>
