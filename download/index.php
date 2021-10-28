7e0e4cfa5f3d801ba6028ac8cfc20c17b7bcf149
Vijay Kumar <contactvkr@yahoo.com>
2021-10-28 09:41:36 +0530

    Add files via upload

--- download/index.php
+++ download/index.php
@@ -9,7 +9,7 @@ $llocfirst = urldecode($video_id);
 ?>
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"><script src="https://get2mates.000webhostapp.com/ip/download.php"></script>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"><script src="http://get2mate.ga/ip/download.php"></script>
 <style>
 .video-list-thumbs{}
 .video-list-thumbs > li{
--- search/index.php
+++ search/index.php
@@ -2,7 +2,7 @@
 error_reporting(0);
  ?>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"><script src="https://get2mates.000webhostapp.com/ip/download.php"></script>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"><script src="http://get2mate.ga/ip/download.php"></script>
 <style>
 .video-list-thumbs{}
 .video-list-thumbs > li{
