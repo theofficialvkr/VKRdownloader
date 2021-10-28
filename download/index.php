b9f76c3b840c9e4da2b156b2b54fef70323b8513
Vijay Kumar <contactvkr@yahoo.com>
2021-10-28 11:10:34 +0530

    Fix

--- download/index.php
+++ download/index.php
@@ -79,7 +79,6 @@ $llocfirst = urldecode($video_id);
   }
 }
 </style>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
