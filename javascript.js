17dbaf61cf8a28960e4029cd2098b8905d87820a
Vijay Kumar <contactvkr@yahoo.com>
2022-11-10 20:59:41 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -17,7 +17,7 @@ var myParam = myurl.value;
     name = name.replace(/[\[\]]/g, '\\$&');
     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
         results = regex.exec(url);
-    if (!results) return null;
+    if (!results) return '.';
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
