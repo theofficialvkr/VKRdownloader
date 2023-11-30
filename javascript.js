1a9ad16a049d6f1fd306bc5001585e54f5525573
Vijay Kumar <contactvkr@yahoo.com>
2023-11-30 10:17:52 +0530

    Fix

--- javascript.js
+++ javascript.js
@@ -22,7 +22,7 @@ var myParamV = myurl.value;
 }
 if(myParamV){
 $.ajax({
-    url:"https://www.vkrfork.vercel.app/server/api/trial.php?vkr="+myParamV,
+    url:"https://vkrfork.vercel.app/server/api/trial.php?vkr="+myParamV,
     type:"GET",
     async:true,
     crossDomain:true,
