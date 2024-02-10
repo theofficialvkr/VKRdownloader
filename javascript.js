d686baafd238dfb6d9dcb40cbca79a09a749beee
Vijay Kumar <contactvkr@yahoo.com>
2024-02-11 02:14:47 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -74,7 +74,7 @@ function generateDownloadButtons(data) {
             const downloadUrl = data["dl" + i]["url"];
             const bgColor = getBackgroundColor(getParameterByName("itag", downloadUrl));
             const videoFrmt = data["dl" + i]["format"];
-            downloadV.innerHTML += `<a href='${downloadUrl}'><button class='dlbtn' style='background:${bgColor}'>${videoFrmt}</button></a>`;
+            downloadV.innerHTML += `<a href='${downloadUrl}'><button class='dlbtns' style='background:${bgColor}'>${videoFrmt}</button></a>`;
         }
     }
 
