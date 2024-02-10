739da445dd3878907ab1136ffb38c9d0e6fe7ada
Vijay Kumar <contactvkr@yahoo.com>
2024-02-11 02:22:51 +0530

    Fixed video title

--- javascript.js
+++ javascript.js
@@ -45,7 +45,8 @@ function handleSuccessResponse(data, inputUrl) {
 
         // Update HTML elements with video information
         updateElement("thumb", videoData.thumb ? `<img src='${videoData.thumb}' width='300px'>` : "<img src='logo.png' width='300px'>");
-        updateElement("title", videoData.title ? `<h1>${decodeURIComponent(videoData.title)}</h1>` : "");
+        // Update HTML elements with video information
+        updateElement("title", videoData.title ? `<h1>${videoData.title.replace(/\+/g, ' ')}</h1>` : "");
         document.title = videoData.title ? `Download ${videoData.title} VKrDownloader` : "Download VKrDownloader";
         updateElement("description", videoData.description ? `<h3><details> <summary>View Description</summary>${videoData.description}</details></h3>` : "");
         updateElement("uploader", videoData.source ? `<h5>${videoData.source}</h5>` : "");
