bf829c476021230842deac196a6dc480abebecd9
Vijay Kumar <contactvkr@yahoo.com>
2024-10-25 15:11:21 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -104,7 +104,7 @@ function getParameterByName(name, url) {
  */
 function makeRequest(inputUrl, retries = 4) {
     $.ajax({
-        url: `https://vkrdownloader.xyz/server?vkr=${encodeURIComponent(inputUrl)}&api_key=vkrdownloader`,
+        url: `https://vkrdownloader.vercel.app/server?vkr=${encodeURIComponent(inputUrl)}&api_key=vkrdownloader`,
         type: "GET",
         cache: true,
         async: true,
